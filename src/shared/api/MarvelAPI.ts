c3b2dea4e879f8b98d69060584e4f8378042e596
Alexnader Fagrelius <alexander.fagrelius@drapereurope.se>
2021-11-30 19:03:14 +0100

    https

--- src/shared/api/MarvelAPI.ts
+++ src/shared/api/MarvelAPI.ts
@@ -3,7 +3,7 @@ const _publickey = "daf4024145d4176879bb994889f44c75";
 const _ts = "timestamp";
 const _hash = "da8dbe4c60bf39c30812b3967425f954";
 const TMDBapi = axios.create({
-  baseURL: "http://gateway.marvel.com/v1/public",
+  baseURL: "https://gateway.marvel.com/v1/public",
   params: {
     apikey: _publickey,
     ts: _ts,
